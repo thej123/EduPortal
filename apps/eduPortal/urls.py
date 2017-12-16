78fde346a977b552b8a9c426bf321becdb6b8e8b
Thej <tkodiyat01@gmail.com>
2017-12-16 12:23:36 -0800

    created models.py (user schema), updated urls for registersubmit, connect, find parnter, updated functons for the all the url paths

--- apps/eduPortal/urls.py
+++ apps/eduPortal/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.login),
-    # url(r'^/login$', views.login),
+    url(r'^$', views.index),
+    url(r'^login$', views.login),
     url(r'^register', views.register),
     url(r'^dashboard', views.dashboard),
+    url(r'^registerSubmit', views.registerSubmit),
+    url(r'^connect', views.connect),
+    url(r'^findPartner', views.findPartner),
 ]
